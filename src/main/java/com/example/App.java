edaa6d74fba84bf1eaaf6b3076f5c5e827c01009
jamalMW <jamalmitwally@hotmail.nl>
2024-03-27 20:38:48 +0100

    reubuild app

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,31 +8,21 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
+//deze class start de app op bij de main menu en geeft het venster een naam.
 public class App extends Application {
 
-    private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("MainMenu"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(@SuppressWarnings("exports") Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainMenu.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Jamal Mitwally-2221071 Colin Valster-2174591");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
+   
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
 }
